38a0c5a661a6686d2faf2beb3db82133dc1af01d
Tonito <otano0398@gmail.com>
2020-11-15 19:29:31 -0600

    Discretization and fuzzification in fuzzification interface

---
+++ fuzzification.py
@@ -0,0 +1,8 @@
+from classes import INF
+
+# levels: A list of tuples (min, max, value) which maps the input to value if it belongs to [min, max)
+def discretize_and_normalize(levels, input_value):
+    for _min, _max, value in levels:
+        if input_value >= _min and input_value < _max:
+            return value
+    raise ValueError(f"There is no level to map the input value {input_value} to")
