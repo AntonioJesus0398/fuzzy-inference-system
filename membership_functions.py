67a63eb16d77b57044ab99cf6037cb3701e848bd
Tonito <otano0398@gmail.com>
2020-11-15 19:10:58 -0600

    Trapezoidal membership function

--- membership_functions.py
+++ membership_functions.py
@@ -3,8 +3,19 @@ def triangular(a1, a2, a3):
     def f(x):
         if x >= a1 and x <= a2:
             return (x - a1) / (a2 - a1)
-        elif x >= a2 and x <= a3:
+        if x >= a2 and x <= a3:
             return (a3 - x) / (a3 - a2)
-        else:
-            return 0
+        return 0
     return f
+
+def trapezoidal(a1, a2, a3, a4):
+    def f(x):
+        if x >= a1 and x <= a2:
+            return (x - a1) / (a2 - a1)
+        if x >= a2 and x <= a3:
+            return 1
+        if x >= a3 and x <= a4:
+            return (a4 - x) / (a4 - a3)
+        return 0
+    return f
+
