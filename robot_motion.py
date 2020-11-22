8ec6a2e6d2aa2ee191f3eddd9ce20a70f71f5cb1
Tonito <otano0398@gmail.com>
2020-11-22 12:41:40 -0600

    Created a console application

--- robot_motion.py
+++ robot_motion.py
@@ -57,11 +57,39 @@ for d_term in Distance.terms:
         except KeyError:
             rule_base.add_rule([('distance', d_term), ('angle', a_term)], [('direction', 'zero-turn')])
 
-for control_variable in rule_base.rules:
-    for rule in rule_base.rules[control_variable]:
-        print(rule)
-
 FIS = FuzzyInferenceSystem(rule_base, inference_method="Larsen")
 
-a = FIS.solve([('distance', Distance.build_singleton(8)), ('angle', Angle.build_singleton(88))])
-# print(a)
+class InputError(Exception):
+    pass
+
+while True:
+    while True:
+        distance = input("Introduzca la distancia a la que se encuentra el objeto. (Debe ser un número entre 0 y 100): ")
+        try:
+            distance = int(distance)
+            if distance < 0 or distance > 100: raise InputError()
+            break
+        except:
+            print("La distancia debe de ser un número entre 0 y 100")
+            continue
+
+    while True:
+        angle = input("Introduzca el ángulo que forma el objeto con el robot. (Debe ser un número entre 0 y 180): ")
+        try:
+            angle = int(angle)
+            if angle < 0 or angle > 180: raise InputError()
+            break
+        except:
+            print("El ángulo debe ser un número entre 0 y 180")
+            continue
+
+    result = FIS.solve([('distance', Distance.build_singleton(distance)), ('angle', Angle.build_singleton(angle))])
+    print(f"El ángulo de la dirección que debe tomar el robot es: {result['direction']}")
+
+    while True:
+        choice = input("Desea continuar(s/n)?: ")
+        if choice == 's':
+            print('\n')
+            break
+        if choice == 'n':
+            exit(0)
